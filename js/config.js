b4d5af6a31c0c97d2d7305ac00b17c2580dbda3c
Shubham Malhotra <141633563+Shubhammalhotra001@users.noreply.github.com>
2024-11-19 22:40:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_OtxLuGEbD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2p433n5n56kjio1g2rdltqi357', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_J7CqYEizy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '48qs8d0fkl3ftpe4ev67slnc0f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
